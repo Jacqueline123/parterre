5f69f8eeb25d31199b08fc27472efea9c674ba12
“jasmine” <Jacqueline.grapes@yandex.ru>
2017-03-23 23:52:09 +0300

    hhhhh hhhh hhh hh h

---
+++ js/script.js
@@ -0,0 +1,183 @@
+"use strict";
+
+
+            $(document).ready(function(){
+                $(".prod1").mouseover(function(){
+                    $("#show1").hide();
+                });
+                $(".prod1").mouseover(function(){
+                    $("#hide1").show();
+                });
+            });
+
+            $(document).ready(function(){
+                $(".prod1").mouseout(function(){
+                    $("#show1").show();
+                });
+                $(".prod1").mouseout(function(){
+                    $("#hide1").hide();
+                });
+            });
+
+
+
+
+
+            $(document).ready(function(){
+                $(".prod2").mouseover(function(){
+                    $("#show2").hide();
+                });
+                $(".prod2").mouseover(function(){
+                    $("#hide2").show();
+                });
+            });
+
+            $(document).ready(function(){
+                $(".prod2").mouseout(function(){
+                    $("#show2").show();
+                });
+                $(".prod2").mouseout(function(){
+                    $("#hide2").hide();
+                });
+            });
+
+
+
+
+
+            $(document).ready(function(){
+                $(".prod3").mouseover(function(){
+                    $("#show3").hide();
+                });
+                $(".prod3").mouseover(function(){
+                    $("#hide3").show();
+                });
+            });
+
+            $(document).ready(function(){
+                $(".prod3").mouseout(function(){
+                    $("#show3").show();
+                });
+                $(".prod3").mouseout(function(){
+                    $("#hide3").hide();
+                });
+            });
+
+
+
+
+            $(document).ready(function(){
+                $(".prod4").mouseover(function(){
+                    $("#show4").hide();
+                });
+                $(".prod4").mouseover(function(){
+                    $("#hide4").show();
+                });
+            });
+
+            $(document).ready(function(){
+                $(".prod4").mouseout(function(){
+                    $("#show4").show();
+                });
+                $(".prod4").mouseout(function(){
+                    $("#hide4").hide();
+                });
+            });
+
+
+
+
+            $(document).ready(function(){
+                $(".prod5").mouseover(function(){
+                    $("#show5").hide();
+                });
+                $(".prod5").mouseover(function(){
+                    $("#hide5").show();
+                });
+            });
+
+            $(document).ready(function(){
+                $(".prod5").mouseout(function(){
+                    $("#show5").show();
+                });
+                $(".prod5").mouseout(function(){
+                    $("#hide5").hide();
+                });
+            });
+
+
+
+
+            $(document).ready(function(){
+                $(".prod6").mouseover(function(){
+                    $("#show6").hide();
+                });
+                $(".prod6").mouseover(function(){
+                    $("#hide6").show();
+                });
+            });
+
+            $(document).ready(function(){
+                $(".prod6").mouseout(function(){
+                    $("#show6").show();
+                });
+                $(".prod6").mouseout(function(){
+                    $("#hide6").hide();
+                });
+            });
+
+            $(document).ready(function(){
+                $(".prod7").mouseover(function(){
+                    $("#show7").hide();
+                });
+                $(".prod7").mouseover(function(){
+                    $("#hide7").show();
+                });
+            });
+
+            $(document).ready(function(){
+                $(".prod7").mouseout(function(){
+                    $("#show7").show();
+                });
+                $(".prod7").mouseout(function(){
+                    $("#hide7").hide();
+                });
+            });
+
+            $(document).ready(function(){
+                $(".prod8").mouseover(function(){
+                    $("#show8").hide();
+                });
+                $(".prod8").mouseover(function(){
+                    $("#hide8").show();
+                });
+            });
+
+            $(document).ready(function(){
+                $(".prod8").mouseout(function(){
+                    $("#show8").show();
+                });
+                $(".prod8").mouseout(function(){
+                    $("#hide8").hide();
+                });
+            });
+
+
+
+            $(document).ready(function(){
+                $(".prod9").mouseover(function(){
+                    $("#show9").hide();
+                });
+                $(".prod9").mouseover(function(){
+                    $("#hide9").show();
+                });
+            });
+
+            $(document).ready(function(){
+                $(".prod9").mouseout(function(){
+                    $("#show9").show();
+                });
+                $(".prod9").mouseout(function(){
+                    $("#hide9").hide();
+                });
+            });
